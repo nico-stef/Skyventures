0f85a0caa36a99ae074304f5fad56e3bcef26996
nicoleta <nico.stefanica12@gmail.com>
2024-11-09 09:23:37 +0200

    update login

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const session = require('express-session');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+      secure: false, //true=only https
+      maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+     }
+}));
 
 app.use("/", require("./routes/authRoutes"));
 
